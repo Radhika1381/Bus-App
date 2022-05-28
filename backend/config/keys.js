c8b58aea1b92bc4d80eff3331e3e8ccac0222adb
Radhika1381 <radhika.iyengar_cs20@gla.ac.in>
2022-05-28 16:12:31 +0530

    Co-authored-by: Utkarsh Vishwakarma <Utqrsh04@users.noreply.github.com>

--- backend/config/keys.js
+++ backend/config/keys.js
@@ -1,3 +1,3 @@
 module.exports = {
-    MongoURI: "mongodb+srv://AdityaNew:123@cluster0-rsoqu.mongodb.net/test?retryWrites=true&w=majority"
-}
+    MongoURI: "mongodb://localhost:27017/mydbname",
+};
