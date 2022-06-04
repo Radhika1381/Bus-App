cca85115ae2f72081780f7e32e3110937a9fed73
Radhika1381 <radhika.iyengar_cs20@gla.ac.in>
2022-06-04 13:18:18 +0530

    Final Commit

--- backend/auth/auth.js
+++ backend/auth/auth.js
@@ -33,6 +33,7 @@ passport.use('login', new localStrategy({
 }));
 
 const JWTstrategy = require('passport-jwt').Strategy;
+///////
 //We use this to extract the JWT sent by the user
 const ExtractJWT = require('passport-jwt').ExtractJwt;
 
